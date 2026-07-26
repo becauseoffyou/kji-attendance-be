b7cacf6bd7d07fa9daafa3587d34bde8405c847d
becauseoffyou <irhandy96@gmail.com>
2026-07-26 19:12:53 +0700

    Create Login API

---
+++ controllers/authController.js
@@ -0,0 +1,62 @@
+const pool = require("../config/db");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const result = await pool.query(
+            "SELECT * FROM users WHERE email = $1",
+            [email]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(401).json({
+                success: false,
+                message: "Email tidak ditemukan",
+            });
+        }
+
+        const user = result.rows[0];
+
+        const valid = await bcrypt.compare(password, user.password);
+
+        if (!valid) {
+            return res.status(401).json({
+                success: false,
+                message: "Password salah",
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                id: user.id,
+                role: user.role_id,
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "1d",
+            }
+        );
+
+        res.json({
+            success: true,
+            token,
+            user: {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                role: user.role_id,
+            },
+        });
+
+    } catch (err) {
+        console.error(err);
+
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+        });
+    }
+};
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const pool = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 require("dotenv").config();
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.json({

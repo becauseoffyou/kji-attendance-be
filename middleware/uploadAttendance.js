3d4323d711b9a5d72a0afc320fcedb0dc168a6dd
becauseoffyou <irhandy96@gmail.com>
2026-07-28 16:38:03 +0700

    fixing multer

--- middleware/uploadAttendance.js
+++ middleware/uploadAttendance.js
@@ -1,9 +1,17 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+
+const uploadDir = path.join(process.cwd(), "uploads", "attendance");
+
+// Buat folder jika belum ada
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "uploads/attendance");
+        cb(null, uploadDir);
     },
 
     filename: (req, file, cb) => {
@@ -17,13 +25,11 @@ const storage = multer.diskStorage({
 });
 
 const fileFilter = (req, file, cb) => {
-
     if (file.mimetype.startsWith("image/")) {
         cb(null, true);
     } else {
         cb(new Error("File harus berupa gambar"), false);
     }
-
 };
 
 module.exports = multer({

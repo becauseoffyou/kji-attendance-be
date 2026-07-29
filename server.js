fdc633c5b588e787e783f012e5c38e62a9ea1936
becauseoffyou <irhandy96@gmail.com>
2026-07-29 14:54:20 +0700

    add route gps

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const cors = require("cors");
 const pool = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const attendanceRoutes = require("./routes/attendance.routes");
+const officeRoutes = require("./routes/office");
+
 require("dotenv").config();
-console.log("DATABASE_URL =", process.env.DATABASE_URL);
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/attendance", attendanceRoutes);
+app.use("/api/office", officeRoutes);
 app.get("/", (req, res) => {
     res.json({
         success: true,

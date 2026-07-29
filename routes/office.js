6a626052f9998030adb80fb20081e0a161d6687e
becauseoffyou <irhandy96@gmail.com>
2026-07-29 15:01:46 +0700

    add route gps v2

--- routes/office.js
+++ routes/office.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const authMiddleware = require("../middlewares/authMiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
 const officeController = require("../controllers/officeController");
 
 router.get(

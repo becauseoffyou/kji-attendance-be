4a8e519f9930aedb731e09a3892b14d405f5bd95
becauseoffyou <irhandy96@gmail.com>
2026-08-03 17:55:00 +0700

    add ulloadp file

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -32,6 +33,7 @@ app.use(
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // ==============================
 // API Routes

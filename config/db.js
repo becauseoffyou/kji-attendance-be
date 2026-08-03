dfd8c86edb916aa34faf510a57c305e781885b66
becauseoffyou <irhandy96@gmail.com>
2026-08-03 11:00:21 +0700

    fixing time zone

--- config/db.js
+++ config/db.js
@@ -3,18 +3,21 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false,
-    },
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-pool.on("connect", () => {
-    console.log("✅ PostgreSQL Client Connected");
+pool.on("connect", async (client) => {
+  await client.query("SET TIME ZONE 'Asia/Jakarta'");
+
+  console.log("✅ PostgreSQL Client Connected");
+  console.log("🌏 Timezone: Asia/Jakarta");
 });
 
 pool.on("error", (err) => {
-    console.error("❌ PostgreSQL Pool Error:", err);
+  console.error("❌ PostgreSQL Pool Error:", err);
 });
 
 module.exports = pool;

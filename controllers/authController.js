7564377e27c6d411ae0d6a7c3782ebfeb5022648
becauseoffyou <irhandy96@gmail.com>
2026-08-04 11:38:07 +0700

    change logic approval

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,90 +3,103 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 exports.login = async (req, res) => {
-    try {
-        const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-        const result = await pool.query(
-            "SELECT * FROM users WHERE email = $1",
-            [email]
-        );
+    const result = await pool.query(
+      `
+    SELECT
 
-        if (result.rows.length === 0) {
-            return res.status(401).json({
-                success: false,
-                message: "Email tidak ditemukan",
-            });
-        }
+        u.*,
 
-        const user = result.rows[0];
+        r.name AS role_name
 
-        const valid = await bcrypt.compare(password, user.password);
+    FROM users u
 
-        if (!valid) {
-            return res.status(401).json({
-                success: false,
-                message: "Password salah",
-            });
-        }
+    JOIN roles r
+    ON r.id = u.role_id
 
-        const token = jwt.sign(
-            {
-                id: user.id,
-                role: user.role_id,
-            },
-            process.env.JWT_SECRET,
-            {
-                expiresIn: "1d",
-            }
-        );
+    WHERE u.email = $1
+    `,
+      [email],
+    );
 
-        res.json({
-            success: true,
-            token,
-            user: {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                role: user.role_id,
-            },
-        });
-
-    } catch (err) {
-        console.error(err);
-
-        res.status(500).json({
-            success: false,
-            message: "Internal Server Error",
-        });
+    if (result.rows.length === 0) {
+      return res.status(401).json({
+        success: false,
+        message: "Email tidak ditemukan",
+      });
     }
+
+    const user = result.rows[0];
+
+    const valid = await bcrypt.compare(password, user.password);
+
+    if (!valid) {
+      return res.status(401).json({
+        success: false,
+        message: "Password salah",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        role_id: user.role_id,
+        role: user.role_name,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d",
+      },
+    );
+
+    res.json({
+      success: true,
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+
+        role_id: user.role_id,
+        role: user.role_name,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
 };
 
 exports.me = async (req, res) => {
-    try {
+  try {
+    const result = await pool.query(
+      "SELECT id, role_id, nik, name, email, department, position, photo, status FROM users WHERE id = $1",
+      [req.user.id],
+    );
 
-        const result = await pool.query(
-            "SELECT id, role_id, nik, name, email, department, position, photo, status FROM users WHERE id = $1",
-            [req.user.id]
-        );
-
-        if (result.rows.length === 0) {
-            return res.status(404).json({
-                success: false,
-                message: "User tidak ditemukan"
-            });
-        }
-
-        res.json({
-            success: true,
-            user: result.rows[0]
-        });
-
-    } catch (err) {
-        console.error(err);
-
-        res.status(500).json({
-            success: false,
-            message: "Internal Server Error"
-        });
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "User tidak ditemukan",
+      });
     }
+
+    res.json({
+      success: true,
+      user: result.rows[0],
+    });
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
 };

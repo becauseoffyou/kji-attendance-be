5d5e9291d79a2cd6f3b5ea979fbafc0b16259854
becauseoffyou <irhandy96@gmail.com>
2026-08-12 13:01:28 +0700

    add logic approval edit absen

--- controllers/leaderController.js
+++ controllers/leaderController.js
@@ -1,5 +1,96 @@
 const pool = require("../config/db");
 
+// exports.leaveRequests = async (req, res) => {
+//   try {
+//     const supervisorId = req.user.id;
+
+//     const status = req.query.status || "PENDING_SUPERVISOR";
+
+//     let sql = `
+//       SELECT
+
+//           lr.id,
+//           lr.leave_type,
+//           lr.start_date,
+//           lr.end_date,
+//           lr.reason,
+//           lr.attachment,
+//           lr.status,
+//           lr.created_at,
+//           lr.approved_at,
+
+//           u.id AS employee_id,
+//           u.name,
+//           u.email
+
+//       FROM leave_requests lr
+
+//       JOIN users u
+//       ON u.id = lr.user_id
+
+//       WHERE
+//           u.supervisor_id = $1
+//     `;
+
+//     const params = [supervisorId];
+//     if (status !== "ALL") {
+//       sql += ` AND lr.status = $2`;
+
+//       params.push(status);
+//     }
+
+//     sql += `
+//       ORDER BY lr.created_at DESC
+//     `;
+
+//     const result = await pool.query(sql, params);
+
+//     const summary = await pool.query(
+//       `
+//       SELECT
+
+//           COUNT(*) FILTER (
+//               WHERE lr.status='PENDING_SUPERVISOR'
+//           ) AS pending,
+
+//           COUNT(*) FILTER (
+//               WHERE lr.status='APPROVED'
+//           ) AS approved,
+
+//           COUNT(*) FILTER (
+//               WHERE lr.status='REJECTED'
+//           ) AS rejected,
+
+//           COUNT(*) AS total
+
+//       FROM leave_requests lr
+
+//       JOIN users u
+//       ON u.id = lr.user_id
+
+//       WHERE
+//       u.supervisor_id = $1
+//       `,
+//       [supervisorId],
+//     );
+
+//     return res.json({
+//       success: true,
+
+//       summary: summary.rows[0],
+
+//       data: result.rows,
+//     });
+//   } catch (err) {
+//     console.error(err);
+
+//     return res.status(500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
+
 exports.leaveRequests = async (req, res) => {
   try {
     const supervisorId = req.user.id;
@@ -7,78 +98,154 @@ exports.leaveRequests = async (req, res) => {
     const status = req.query.status || "PENDING_SUPERVISOR";
 
     let sql = `
-      SELECT
+            SELECT
+                lr.id,
+                lr.leave_type,
+                lr.start_date,
+                lr.end_date,
+                lr.reason,
+                lr.attachment,
+                lr.status,
+                lr.created_at,
+                lr.approved_at,
 
-          lr.id,
-          lr.leave_type,
-          lr.start_date,
-          lr.end_date,
-          lr.reason,
-          lr.attachment,
-          lr.status,
-          lr.created_at,
-          lr.approved_at,
+                u.id AS employee_id,
+                u.name,
+                u.email,
 
-          u.id AS employee_id,
-          u.name,
-          u.email
+                'LEAVE' AS request_type,
 
-      FROM leave_requests lr
+                NULL::timestamp AS old_check_in,
+                NULL::timestamp AS new_check_in,
+                NULL::timestamp AS old_check_out,
+                NULL::timestamp AS new_check_out
 
-      JOIN users u
-      ON u.id = lr.user_id
+            FROM leave_requests lr
 
-      WHERE
-          u.supervisor_id = $1
-    `;
+            JOIN users u
+                ON u.id = lr.user_id
+
+            WHERE
+                u.supervisor_id = $1
+        `;
 
     const params = [supervisorId];
+
     if (status !== "ALL") {
       sql += ` AND lr.status = $2`;
-
       params.push(status);
     }
 
     sql += `
-      ORDER BY lr.created_at DESC
-    `;
+
+            UNION ALL
+
+            SELECT
+                aer.id,
+
+                'PERUBAHAN ABSENSI' AS leave_type,
+
+                a.attendance_date AS start_date,
+                a.attendance_date AS end_date,
+
+                aer.reason,
+
+                NULL AS attachment,
+
+                aer.status,
+                aer.created_at,
+                aer.approved_at,
+
+                u.id AS employee_id,
+                u.name,
+                u.email,
+
+                'ATTENDANCE_EDIT' AS request_type,
+
+                aer.old_check_in,
+                aer.new_check_in,
+                aer.old_check_out,
+                aer.new_check_out
+
+            FROM attendance_edit_requests aer
+
+            JOIN users u
+                ON u.id = aer.user_id
+
+            JOIN attendance a
+                ON a.id = aer.attendance_id
+
+            WHERE
+                u.supervisor_id = $1
+        `;
+
+    if (status !== "ALL") {
+      sql += ` AND aer.status = $2`;
+    }
+
+    sql += `
+            ORDER BY created_at DESC
+        `;
 
     const result = await pool.query(sql, params);
 
+    // ============================
+    // SUMMARY
+    // ============================
+
     const summary = await pool.query(
       `
-      SELECT
+            SELECT
+                COUNT(*) FILTER (
+                    WHERE status = 'PENDING_SUPERVISOR'
+                ) AS pending,
 
-          COUNT(*) FILTER (
-              WHERE lr.status='PENDING_SUPERVISOR'
-          ) AS pending,
+                COUNT(*) FILTER (
+                    WHERE status = 'APPROVED'
+                ) AS approved,
 
-          COUNT(*) FILTER (
-              WHERE lr.status='APPROVED'
-          ) AS approved,
+                COUNT(*) FILTER (
+                    WHERE status = 'REJECTED'
+                ) AS rejected,
 
-          COUNT(*) FILTER (
-              WHERE lr.status='REJECTED'
-          ) AS rejected,
+                COUNT(*) AS total
 
-          COUNT(*) AS total
+            FROM (
 
-      FROM leave_requests lr
+                SELECT
+                    lr.status
 
-      JOIN users u
-      ON u.id = lr.user_id
+                FROM leave_requests lr
 
-      WHERE
-      u.supervisor_id = $1
-      `,
+                JOIN users u
+                    ON u.id = lr.user_id
+
+                WHERE
+                    u.supervisor_id = $1
+
+
+                UNION ALL
+
+
+                SELECT
+                    aer.status
+
+                FROM attendance_edit_requests aer
+
+                JOIN users u
+                    ON u.id = aer.user_id
+
+                WHERE
+                    u.supervisor_id = $1
+
+            ) requests
+            `,
       [supervisorId],
     );
 
     return res.json({
       success: true,
-
       summary: summary.rows[0],
-
       data: result.rows,
     });
   } catch (err) {
@@ -90,7 +257,6 @@ exports.leaveRequests = async (req, res) => {
     });
   }
 };
-
 exports.leaveDetail = async (req, res) => {
   try {
     const { id } = req.params;

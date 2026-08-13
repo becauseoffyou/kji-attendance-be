bd928018021f789405a39480ffbd9b1978cfcccc
becauseoffyou <irhandy96@gmail.com>
2026-08-13 12:10:50 +0700

    email test bug google api

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const controller = require("../controllers/testController");
 
+router.get("/email", controller.testEmail);
 router.get("/google", controller.googleAuth);
 
 router.get("/google/callback", controller.googleCallback);

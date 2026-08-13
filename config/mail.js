85102dc57086d22cba4f9e9600be3c384ca25e7b
becauseoffyou <irhandy96@gmail.com>
2026-08-13 11:29:43 +0700

    email test v2

--- config/mail.js
+++ config/mail.js
@@ -1,12 +1,20 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+
+  family: 4,
 
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASSWORD,
   },
+
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 15000,
 });
 
 module.exports = transporter;
